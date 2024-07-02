06f45b260dcd5b85fa9e560c5907af57e2686383
Adeebahmed333 <adeebahmed3337@gmail.com>
2024-07-02 17:53:35 +0530

    Made basic socket connection from client and server

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const express =require('express');
-const dotenv =require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const http = require("http");
+const { Server } = require("socket.io");
+
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.on('from_client',()=>{
+    console.log('Event recieved from client');
+  });
+  setInterval(()=>{
+    socket.emit('from_server')
+  },3000);
+});
+
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
